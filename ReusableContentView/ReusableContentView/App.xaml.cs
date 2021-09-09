98816db691178e0a4dcceb7ec9081eb4fcc4985a
LeiscarT <leiscartp29@gmail.com>
2021-09-09 10:03:00 -0400

    initial commit

---
+++ ReusableContentView/ReusableContentView/App.xaml.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace ReusableContentView
+{
+    public partial class App : Application
+    {
+        public App()
+        {
+            InitializeComponent();
+
+            MainPage = new MainPage();
+        }
+
+        protected override void OnStart()
+        {
+        }
+
+        protected override void OnSleep()
+        {
+        }
+
+        protected override void OnResume()
+        {
+        }
+    }
+}

68fff32482503b0eb8e058ec7783ae430cf277b7
LeiscarT <leiscartp29@gmail.com>
2021-09-09 12:37:44 -0400

    add images and adjust main page

--- ReusableContentView/ReusableContentView/ViewModels/MenuViewModel.cs
+++ ReusableContentView/ReusableContentView/ViewModels/MenuViewModel.cs
@@ -17,12 +17,12 @@ namespace ReusableContentView.ViewModels
 
         public MenuViewModel()
         {
-            Item1 = new Card("Bar & Hotels", "beach.png", "42 places");
-            Item2 = new Card("Fine Dining", "beach.png", "16 places");
-            Item3 = new Card("Coffes", "beach.png", "20 places");
-            Item4 = new Card("Nearby", "beach.png", "80 places");
-            Item5 = new Card("Fast Food", "beach.png", "17 places");
-            Item6 = new Card("Featured Foods", "beach.png", "12 places");
+            Item1 = new Card("Bar & Hotels", "bar.png", "42 places");
+            Item2 = new Card("Fine Dining", "dining.png", "16 places");
+            Item3 = new Card("Coffees", "coffee.png", "20 places");
+            Item4 = new Card("Nearby", "near.png", "80 places");
+            Item5 = new Card("Fast Food", "food.png", "17 places");
+            Item6 = new Card("Featured Foods", "favoritefood.png", "12 places");
         }
 
         public event PropertyChangedEventHandler PropertyChanged;

12a747f47c4adb64c11cf3eaf0c9b68eed222090
LeiscarT <leiscartp29@gmail.com>
2021-09-09 10:16:07 -0400

    add card model

---
+++ ReusableContentView/ReusableContentView/Models/Card.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ReusableContentView.Models
+{
+    class Card
+    {
+        public string Title { get;}
+        public string Image { get;}
+        public string Detail  {get;}
+
+        public Card(string title, string image, string detail)
+        {
+            Title = title;
+            Image = image;
+            Detail = detail;
+        }
+
+    }
+}
